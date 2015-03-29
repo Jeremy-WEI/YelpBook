9e9847cf6f786ecb77ec0f901b83ef033bfafaa6
dichenli <dichenli@sas.upenn.edu>
2015-03-29 19:54:20 -0400

    try sql

--- yelpbook/routes/users.js
+++ yelpbook/routes/users.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+console.log('mysql');
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+	host     : 'mydatabase.cfxag8k1xo7h.us-east-1.rds.amazonaws.com',
+	user     : 'linjie',
+	password : '5data5base0',
+	database : 'YelpBook'
 });
 
+
+function query(res) {
+	connection.connect();
+	connection.query('SELECT COUNT(*) from BUSINESS', 
+		function(err, rows, fields) {
+			if (!err) {
+				console.log('The solution is: ', rows);
+				res.send(rows);
+			} else {
+				console.log('Error while performing Query.');
+			}
+			connection.end();
+		});
+}
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+	query(res);
+});
+
+
 module.exports = router;
+
