50f7d774eeb94fc47903110d2046ebf2a1e0d71e
Yunchen Wei <weiyunchen0807@outlook.com>
2015-04-10 16:03:20 -0400

    UI added

--- yelpbook/routes/business.js
+++ yelpbook/routes/business.js
@@ -8,16 +8,23 @@ var connection = mysql.createConnection({
 	database : 'YelpBook'
 });
 
+function doReviewQuery(req, res, busInfo, categories, next) {
+    connection.query('SELECT * FROM REVIEW INNER JOIN USER ON REVIEW.user_id = USER.user_id WHERE REVIEW.business_id = "' + req.query.business_id + '"',
+        function(err, reviews) {
+            if (!err)
+                res.render('business', {business: busInfo, categories: categories, reviews: reviews});
+            else
+                next(new Error(500));
+        });
+}
+
 function doCategoryQuery(req, res, busInfo, next) {
 	connection.query('SELECT category FROM CATEGORY WHERE business_id = "' + req.query.business_id + '"', 
 	function(err, categories) {
 		if (!err)
-			res.render('business', {business: busInfo, categories: categories});
-			// res.send(rows);
+			doReviewQuery(req, res, busInfo, categories, next)
 		else
 			next(new Error(500));
-			// console.log('Error while performing Query.');
-		// connection.end();
 	});
 }
 
@@ -26,16 +33,12 @@ function doBusinessQuery(req, res, next) {
 	connection.query('SELECT * FROM BUSINESS WHERE business_id = "' + req.query.business_id + '"', 
 	function(err, busInfo) {
 		if (!err) {
-			if (busInfo.length == 0) {
-				connection.end();
+			if (busInfo.length == 0)
 				next(new Error(404));
-			}
-			else 
+			else
 				doCategoryQuery(req, res, busInfo, next);
-			// res.send(rows);
 		} else
 			next(new Error(500));
-			// connection.end();
 	});
 }
 
