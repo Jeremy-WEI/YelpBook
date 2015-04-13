56600d5b27d478427eeaa31e6cf79985cd7fb431
Wu Jingyuan <katiewu0304@gmail.com>
2015-04-12 22:13:41 -0400

    with friends

--- yelpbook/routes/users.js
+++ yelpbook/routes/users.js
@@ -5,7 +5,8 @@ var connection = mysql.createConnection({
     host     : 'mydatabase.cfxag8k1xo7h.us-east-1.rds.amazonaws.com',
     user     : 'linjie',
     password : '5data5base0',
-    database : 'YelpBook'
+    database : 'YelpBook',
+    multipleStatements: true
 });
 
 var moment = require('moment'); //time formatter
@@ -19,16 +20,16 @@ var Grid = require('gridfs-stream'); //mongoDB file IO
 var gridfs; //mongoDB file IO, bounded to var DB
 // Retrieve
 
-//console.log("__dirname: " + __dirname);
+//// console.log("__dirname: " + __dirname);
 
 // Connect to the db
 MongoClient.connect("mongodb://dichenli:5data5base0@ds061671.mongolab.com:61671/yelpbook_mongo", function(err, db) {
     if(err) {
-        console.log("mongo connection failed");
-        console.log(err.message);
+        // console.log("mongo connection failed");
+        // console.log(err.message);
         throw err; //connection failed, kill the whole server
     } else {
-        console.log("mongo connection opened");
+        // console.log("mongo connection opened");
         DB = db;
         gridfs = Grid(DB, mongo);
     }
@@ -45,10 +46,10 @@ function redirectLogin(res) {
 
 
 function undefined(obj) {
-    console.log("======undefined?");
-    console.log(obj);
-    console.log(typeof obj === 'undefined');
-    console.log("undefined=====");
+    // console.log("======undefined?");
+    // console.log(obj);
+    // console.log(typeof obj === 'undefined');
+    // console.log("undefined=====");
     return typeof obj === 'undefined';
 }
 
@@ -108,7 +109,7 @@ function redirectUser(res, user_id) {
 }
 //call back function to show my news feed
 function myFeed(req, res, next, err, userid, msg) {
-    console.log("myFeed callback found userid: " + userid);
+    // console.log("myFeed callback found userid: " + userid);
     getPostsQuery(req, res, next, err, userid, msg);
 }
 
@@ -116,13 +117,16 @@ function myFeed(req, res, next, err, userid, msg) {
 //call back function to process a new post
 function newPost(req, res, next, err, userid, msg) {
     if (!undefined(global.done) && global.done != true) { //file upload not done
-        console.log("File uploaded done==false");
+        // console.log("File uploaded done==false");
     } else {
-        var uid = userid[0].user_id;
+//        var uid = userid[0].user_id;
+        console.log(req.body);
+        var uid = userid;
         var post_text = req.body.new_post;
         var datetime = moment().format('YYYY-MM-DD HH:mm:ss');
         var file = req.file;
         var photo_name;
+        var friend_list = req.body.friend;
         if(undefined(file)) {
             photo_name = "NULL";
             global.done = true;
@@ -130,7 +134,17 @@ function newPost(req, res, next, err, userid, msg) {
             photo_name = uid + "_" + file.name;
         }
         var query = "INSERT INTO POST (user_id, text, datetime, photo_name) VALUES ('" + uid + "', '" + post_text + "', '" + datetime + "', '" + photo_name + "')";
+
+        var query_with_friend = 'INSERT INTO WITH_FRIEND (user_id, post_datetime, friend_id) VALUES ';
+        for(var i=0;i<friend_list.length;i++){
+            var tuple = "("+uid+", \""+datetime+"\", "+friend_list[i]+")";
+            if(i==0) query_with_friend = query_with_friend + tuple;
+            else query_with_friend = query_with_friend + ","+tuple;
+        }
+
+
         console.log(query);
+        // console.log(query);
         connection.query(query,
             function(err, results) { //query returns
                 if (err)
@@ -148,7 +162,7 @@ function newPost(req, res, next, err, userid, msg) {
                     });
                     fs.createReadStream(file.path).pipe(writeStream);
                     writeStream.on("close", function () {
-                        console.log("file pipe to DB done");
+                        // console.log("file pipe to DB done");
                         fs.unlinkSync(__dirname + "/../uploads/" + file.name); //delete the file in local uploads
                         getPostsQuery(req, res, next, err, uid, "The post was sent successfully!");
                     });
@@ -164,7 +178,7 @@ function getUserQuery(req, res, next, msg, callBack) {
     if(undefined(req.user)){
         redirectLogin(res);
     }
-    console.log("getUserQuery fb: " + req.user.id); //get the user who sent request
+    // console.log("getUserQuery fb: " + req.user.id); //get the user who sent request
     var fb = req.user.id;
     var query_find_userid = "SELECT user_id FROM USER WHERE fb_account=" + fb;
     console.log(query_find_userid);
@@ -179,33 +193,117 @@ function getUserQuery(req, res, next, msg, callBack) {
     });
 }
 
-function renderUserPosts(res, uid, results,  msg) {
-    res.render('user', {"user_id" : uid, "results": results, "message": msg});
-}
-
-function getPostsQuery(req, res, next, err, user_id, msg) {
-    console.log("getPostsQuery user_id: " + user_id);
-//    var fb_name = null;
-//    var uid = connection.escape(user_id[0]);
-//    console.log("uid: " + uid);
-    var uid = user_id;
-    var query = "SELECT * FROM USER INNER JOIN ((SELECT * FROM POST WHERE user_id IN"
-    + "(SELECT user_id2 as user_id FROM FRIEND WHERE user_id1='" + uid + "'))"
-    + "UNION"
-    + "(SELECT * FROM POST WHERE user_id IN"
-    + "(SELECT user_id1 as user_id FROM FRIEND WHERE user_id2=" + uid + "))"
-    + "UNION"
-    + "(SELECT * FROM POST WHERE user_id IN (SELECT " + uid
-    + "))) AS POSTS ON USER.user_id = POSTS.user_id";
-    console.log(query);
-    connection.query(query, function(err, results) {
-            if (err)
-                renderUserPosts(res, uid, results, "Get Posts failed!");
+function renderUserPosts(res, uid, user_name, results,  msg) {
+    var query_friend_list = 'SELECT user_id2 FROM FRIEND WHERE user_id1='+uid;
+    console.log(query_friend_list);
+    connection.query(query_friend_list, function(err, friends) {
+            if (err){
+                console.log("error");
+                res.render('user', {"user_id" : uid,  "results": results, "message": msg});
+            }
             else {
-                renderUserPosts(res, uid, results, msg);
+                // find the name of the friend
+                var user_id_list = [];
+                var str = "";
+                for(var i=0;i<friends.length;i++){
+                    user_id_list.push(friends[i].user_id2);
+                    if(str == "") str = friends[i].user_id2;
+                    else str = str + ","+ friends[i].user_id2;
+                }
+                str = "("+str+")";
+                var query_find_name = 'SELECT user_id, fb_name FROM USER WHERE user_id in '+str;
+                console.log(query_find_name);
+                connection.query(query_find_name, function(err, friendname){
+                    if(err){
+                        res.render('user', {"user_id" : uid,  "user_name": user_name, "results": results, "message": msg});
+                    }
+                    else{
+                        console.log(friendname);
+                        res.render('user', {"user_id" : uid, "user_name": user_name, "friends" : friendname, "results": results, "message": msg});
+                    }
+                });
+
+
             }
         }
     );
+}
+
+
+function getPostsQuery(req, res, next, err, user_id, msg) {
+    // console.log("getPostsQuery user_id: " + user_id);
+//    var fb_name = null;
+//    var uid = connection.escape(user_id[0]);
+//    // console.log("uid: " + uid);
+    var uid = user_id;
+    var query = 'SELECT FF.user_id, FF.fb_name, text, datetime, photo_name, friend_id, USER.fb_name as friend_fb_name FROM (SELECT R.user_id, USER.fb_name, text, datetime, photo_name, friend_id FROM (SELECT P.user_id as user_id, P.text as text, P.datetime as datetime,'
+    + 'P.photo_name as photo_name, W.friend_id as friend_id FROM (SELECT * FROM (SELECT * FROM POST WHERE POST.user_id='+uid+' OR POST.user_id IN '
+    + '(SELECT FRIEND.user_id2 FROM USER INNER JOIN FRIEND ON USER.user_id=FRIEND.user_id1)) as buffer) P '
+    + 'LEFT OUTER JOIN WITH_FRIEND W on P.user_id=W.user_id and P.datetime=W.post_datetime) R '
+    + 'INNER JOIN USER ON R.user_id = USER.user_id) FF LEFT OUTER JOIN USER on FF.friend_id = USER.user_id ORDER BY user_id, datetime';
+    console.log(query);
+    // console.log(query);
+    connection.query(query, function(err, results) {
+            if (err)
+                renderUserPosts(res, uid, "", results, "Get Posts failed!");
+            else {
+                var user_id;
+                var user_name;
+                var text;
+                var datetime;
+                var photo_name;
+                var friend_id;
+                var friend_name;
+                var finalresults = [];
+                var friend_list = [];
+                for(var i=0;i<results.length;i++) {
+                    if (results[i].user_id == user_id && String(results[i].datetime) == String(datetime)) {
+                        // same post
+                        if(friend_name != null) friend_list.push([results[i].friend_id, results[i].friend_fb_name]);
+                    }
+                    else {
+                        if (user_id != undefined) {
+                            var post = {
+                                "user_id": user_id,
+                                "user_name": user_name,
+                                "text": text,
+                                "datetime": datetime,
+                                "photo_name": photo_name,
+                                "friend_list": friend_list
+                            }
+                            finalresults.push(post);
+                        }
+                        user_id = results[i].user_id;
+                        user_name = results[i].fb_name;
+                        text = results[i].text;
+                        datetime = results[i].datetime;
+                        console.log("new datetime");
+                        console.log(datetime);
+                        photo_name = results[i].photo_name;
+                        friend_id = results[i].friend_id;
+                        friend_name = results[i].friend_fb_name;
+                        friend_list = [];
+                        if(friend_name != null) {
+                            friend_list.push([friend_id, friend_name]);
+                        }
+                    }
+                }
+                var finalpost = {
+                    "user_id": user_id,
+                    "user_name": user_name,
+                    "text": text,
+                    "datetime": datetime,
+                    "photo_name": photo_name,
+                    "friend_list": friend_list
+                }
+                finalresults.push(finalpost);
+                console.log(finalresults);
+                renderUserPosts(res, uid, user_name, finalresults, msg);
+            }
+        }
+    );
+
+
 }
 //
 //function newPost(req, res, next) {
@@ -221,23 +319,23 @@ function getPostsQuery(req, res, next, err, user_id, msg) {
 //get photo from MongoDB
 function getPhotoQuery(req, res, next) {
     var photo_name = req.params.id;
-    console.log(photo_name);
+    // console.log(photo_name);
     if(undefined(photo_name)) {
-        console.log("photo_name === 'undefined'");
+        // console.log("photo_name === 'undefined'");
         next(new error(404));
     }
     var collection = gridfs.files;
-    console.log("collection" + collection);
+    // console.log("collection" + collection);
     collection.findOne({"filename": photo_name}, function(err, item) {
         if(err || !item) {
-            console.log("can't find file from mongoDB fs.files");
+            // console.log("can't find file from mongoDB fs.files");
             next(new Error(404));
         } else {
-            console.log("findone callback");
+            // console.log("findone callback");
             var length = item.length;
             var type = mime.lookup(photo_name);
-            console.log(length);
-            console.log(type);
+            // console.log(length);
+            // console.log(type);
             res.writeHead(200, {
                 'Content-Type': type,
                 'Content-Length': length
@@ -247,10 +345,10 @@ function getPhotoQuery(req, res, next) {
             var readstream = gridfs.createReadStream({
                 filename: photo_name
             });
-            console.log("done find readstream");
+            // console.log("done find readstream");
             readstream.pipe(res);
             readstream.on('error', function(err) {
-                console.log('file stream error!');
+                // console.log('file stream error!');
                 res.end(err);
             })
         }
@@ -282,7 +380,7 @@ router.get('/homepage', function(req, res, next) {
 
 
 router.get('/images/:id', function(req, res, next) {
-    console.log("/images/:id");
+    // console.log("/images/:id");
     getPhotoQuery(req, res, next);
 });
 
